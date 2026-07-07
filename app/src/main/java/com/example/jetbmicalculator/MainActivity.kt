f43f190074c4d110d7faddca9beccfbc92a1e0a4
j-sun <j-sun@esol.co.jp>
2026-07-07 12:44:27 +0900

    Initial commit BMI calculator change 4

--- app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.jetbmicalculator
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +15,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -24,14 +29,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
 import com.example.jetbmicalculator.ui.theme.JetBMICalculatorTheme
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Log.d("Test", viewModel.test)
         enableEdgeToEdge()
         setContent {
             JetBMICalculatorTheme {
@@ -50,6 +60,51 @@ class MainActivity : ComponentActivity() {
                         fontWeight = FontWeight.ExtraBold
                     )
                         Spacer(modifier = Modifier.height(30.dp))
+                        PinkLabeledTextField(
+                            value = viewModel.height,
+                            onValueChange = { viewModel.height = it},
+                            label = "height(cm)",
+                            placeholder = "170",
+                            )
+                        Spacer(modifier = Modifier.height(30.dp))
+                        PinkLabeledTextField(
+                            value = viewModel.weight,
+                            onValueChange = {viewModel.weight = it},
+                            label = "weight(cm)",
+                            placeholder = "60",
+                            )
+                        Spacer(modifier = Modifier.height(30.dp))
+
+                        //calculate
+                        Button(
+                            onClick = {
+                                viewModel.calculateBMI()
+                            },
+                            modifier = Modifier.fillMaxWidth(),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(0xFFF85F6A)
+
+                            )
+                        ) {Text(
+                            text = "calculate",
+                            color = Color.White,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Text(
+                            text = "Your BMI is ${viewModel.bmi}",
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center,
+                            color = Color.Gray,
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                        )
+
+
 
 
 
@@ -86,7 +141,8 @@ fun PinkLabeledTextField(
             value = value,
             onValueChange = onValueChange,
             colors = TextFieldDefaults.colors(
-                focusedTextColor  = Color.Transparent
+                focusedContainerColor  = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
             ),
             placeholder = { Text(text = placeholder) },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
